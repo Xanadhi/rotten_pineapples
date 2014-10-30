df11014fd442bdfed3f163966781a8b8b68d484a
Andy Tran <transient.tran@gmail.com>
2014-10-29 19:10:52 -0700

    Added image uploader feature with carrierwave gem

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141029232741) do
+ActiveRecord::Schema.define(version: 20141030013208) do
 
   create_table "movies", force: true do |t|
     t.string   "title"
     t.string   "director"
     t.integer  "runtime_in_minutes"
     t.text     "description"
-    t.string   "poster_image_url"
+    t.string   "image"
     t.datetime "release_date"
     t.datetime "created_at"
     t.datetime "updated_at"
